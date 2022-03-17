8babb53e869dceb37963055a32bf5fe73ac30e6e
Gonzalo <gondev17@gmail.com>
2022-03-16 22:43:07 -0300

    first commit

---
+++ src/main/java/com/example/pruebatecnicaspringdeveloperionix/PruebaTecnicaSpringDeveloperIonixApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruebatecnicaspringdeveloperionix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSpringDeveloperIonixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaTecnicaSpringDeveloperIonixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pruebatecnicaspringdeveloperionix/PruebaTecnicaSpringDeveloperIonixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruebatecnicaspringdeveloperionix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSpringDeveloperIonixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
